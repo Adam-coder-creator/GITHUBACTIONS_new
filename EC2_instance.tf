08ec1eee8a0a6a5c3491f2bc0e7368643762b91f
Adam <adamtomczyk.95@gmail.com>
2024-07-12 01:31:14 +0200

    Remove comment resources

--- EC2_instance.tf
+++ EC2_instance.tf
@@ -8,21 +8,3 @@ resource "aws_instance" "new_ec2"{
     subnet_id = aws_subnet.new[count.index].id
     vpc_security_group_ids = [aws_security_group.allow_tls.id]
 }
-
-
-
-
-
-
-
-
-# Creating EC2 instance
-/*resource "aws_instance" "web" {
-  ami           = "ami-07c8c1b18ca66bb07"
-  instance_type = "t3.small"
-  key_name = "nowa para"
-  associate_public_ip_address = true
-  subnet_id = aws_subnet.main.id
-  vpc_security_group_ids = [aws_security_group.allow_tls.id]
-}
-*/
