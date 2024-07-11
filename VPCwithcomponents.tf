fd8d4ea8ab0ee720069251626e92163f5b12b0d7
Adam <adamtomczyk.95@gmail.com>
2024-07-12 01:29:38 +0200

    Remove comment resources

--- VPCwithcomponents.tf
+++ VPCwithcomponents.tf
@@ -10,30 +10,6 @@ tags = {
 }
 
 }
-
-#Creating two subnets inside VPC
-/*resource "aws_subnet" "new_part1"{
-    vpc_id = aws_vpc.new.id
-    cidr_block = "10.0.1.0/24"
-    map_public_ip_on_launch = true
- 
-    tags = {
-        Name = "Main_subnet_1"
- }
-
-}
-
-resource "aws_subnet" "new_part2"{
-    vpc_id = aws_vpc.new.id 
-    cidr_block = "10.0.2.0/24"
-    map_public_ip_on_launch = true
-
-    tags = {
-        Name = "Main_subnet_2"
-    }
-
-}
-*/
 resource "aws_subnet" "new" {
   count = length(var.cidr_block_01)
 
