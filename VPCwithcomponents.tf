e175ff8f068eefd266f1e5c4ff610bf4e9975c5d
Adam <adamtomczyk.95@gmail.com>
2024-07-12 00:27:22 +0200

    Add outbound rules

--- VPCwithcomponents.tf
+++ VPCwithcomponents.tf
@@ -2,6 +2,7 @@
 #Creating VPC
 resource "aws_vpc" "new" {
     cidr_block = "10.0.0.0/16"
+    enable_dns_hostnames = true
     #region = "eu-north-1"
 
 tags = {
@@ -38,6 +39,7 @@ resource "aws_subnet" "new" {
 
   vpc_id     = aws_vpc.new.id
   cidr_block = var.cidr_block_01[count.index]
+  map_public_ip_on_launch = true
 
   tags = {
     Name = var.name_01[count.index]
@@ -109,4 +111,14 @@ resource "aws_vpc_security_group_ingress_rule" "allow_tls_2"{
     tags ={
         Name = "Second_rule"
     }
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_outbound" {
+  security_group_id = aws_security_group.allow_tls.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1" 
+
+  tags ={
+    Name = "First_egress"
+  }
 }
