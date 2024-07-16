5e7a2cbf55f4a6f9b2273288c81de47e957d031e
Adam <adamtomczyk.95@gmail.com>
2024-07-16 19:23:46 +0200

    Correct usage of variables

--- variable.tf
+++ variable.tf
@@ -1,34 +1,15 @@
+#Variable to set the cidr_block of the subnet
 variable "cidr_block_01" {
   type    = list(string)
   #default = ["10.0.1.0/24", "10.0.2.0/24"]
 }
-
-#variable "name_01" {
-  #type    = list(string)
- # default = ["Main_subnet_1", "Main_subnet_2"]
-#}
-/*
-variable "vpc_cidr_block" {
-  type        = string
-  description = "The CIDR block of the VPC."
-}
-*/
+#Variable to set the name of the subnets
 variable "name_01" {
   type        = list(string)
   description = "The name of the subnet."  
+  #deafult = ["subnet-1" , "subnet-2"]
 }
-/*
-variable "cidr_block_01" {
-  type        = string
-  description = "The CIDR block of the subnet."
-  validation {
-   # condition     = cidrsubnet(var.cidr_block_01, 8, 0) #== var.cidr_block_01
-    condition     = can(regex("^\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}/\\d{1,2}$", var.cidr_block_01))
-    error_message = "The subnet CIDR block must be a valid subnet within the VPC CIDR block."
-  }
-}
-*/
-
+#Variable to set the AMI which we prefer
 variable "ami" {
   type        = string
   description = "The id of the machine image (AMI) to use for the server."
